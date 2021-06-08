328db63718a1f309dd5aea7211cc8ca5f3615413
ko900321 <ko900321@gmail.com>
2021-06-08 16:18:59 +0800

    Route for GET/apiBooks, GET /apiBooks/add

--- routes/apiBooks.js
+++ routes/apiBooks.js
@@ -1,17 +1,27 @@
 var express = require('express');
+const fetch = require('node-fetch');
 var router = express.Router();
 
 router.get('/', async function (req, res, next) {
-    let data;
     try {
-      const [rows] = await db.query('SELECT * FROM books ORDER BY id desc');
-      data = rows;
-      // res.json(data);
-      res.render('books', { data });
+        const response = await fetch('http://localhost:1337/books');
+        data = await response.json();
+        //const [rows] = await db.query('SELECT * FROM books ORDER BY id desc');
+        
+        // res.json(data);
+        res.render('apiBooks/index', { data });
     } catch (err) {
-      console.log('Errors on getting books!');
-      res.render('books', { data: '' });
+        console.log('Errors on getting books!');
+        res.render('apiBooks/index', { data: '' });
     }
+  });
+
+  router.get('/add', async function (req, res, next) {
+    //res.send('display add book page')
+    res.render('apiBooks/add',{
+      name: '',
+      author:'',
+    });
   });
 
 module.exports = router;
